defb876b3960ca1d09b3f90d8e7c8d0b0c33b4de
Anandha Sudhan <rasudhan@gmail.com>
2018-10-04 21:30:45 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,3 +3,8 @@ var playlist = {a1:"s1"};
 function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName]=songTitle
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist[artistName]
+  return playlist
+}
